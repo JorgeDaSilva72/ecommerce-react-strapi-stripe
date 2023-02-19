34b41a1709738aafa702db17f7042ade8b727b36
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-02-19 17:28:37 +0100

    Creation des composants Card et FeaturedProducts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
   );
 };

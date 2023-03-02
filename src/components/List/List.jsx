ce30a092fc7c0ff166d0ff327f810255696689ce
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-03-02 22:21:57 +0100

    Mise en place des fonctionnalités de selection par catégorie, prix et trie dans le composant products

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,52 +1,20 @@
 import React from "react";
 import "./List.scss";
 import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long T-shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long T-shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long T-shirt",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long T-shirt",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
 
   return (
     <div className="list">
-      {data?.map((item) => (
-        <Card item={item} key={item.id} />
-      ))}
+      {loading
+        ? "loading"
+        : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
   );
 };

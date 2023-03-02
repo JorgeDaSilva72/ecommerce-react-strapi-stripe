cb0a73ed44b1faa43222ec485ef05fecccfc4a86
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-03-02 16:05:24 +0100

    Creation des composants Products et List

---
+++ src/components/List/List.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./List.scss";
+import Card from "../Card/Card";
+
+const List = ({ subCats, maxPrice, sort, catId }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long T-shirt",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long T-shirt",
+      isNew: false,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="list">
+      {data?.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default List;
